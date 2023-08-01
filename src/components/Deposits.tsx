a97b470c0afdf7a1113f7f192cbc078f175b51c2
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-07-31 23:11:34 -0700

    get recent sales hooked up to mock data

--- src/components/Deposits.tsx
+++ src/components/Deposits.tsx
@@ -1,18 +1,28 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
+import { mockTransactions } from './mockdata'
 
 function preventDefault(event: React.MouseEvent) {
   event.preventDefault();
 }
 
 export default function Deposits() {
+  let sum = 0;
+
+  mockTransactions.forEach((a) => {
+    sum += parseFloat(a.cost)
+    console.log(a.cost)
+    console.log(sum)
+  })
+
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
+      <Title>Recent Sales</Title>
       <Typography component="p" variant="h4">
-        $3,024.00
+        ${sum}
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
         on 15 March, 2019
